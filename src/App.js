0772ba39644058b3d83b8aef71f3cfa2b38ad1f3
Agata Urbańska <agata.urbanska9@gmail.com>
2020-05-23 00:04:58 +0200

    routing /Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {Home} from './components/Home'
+import {OddajRzeczy} from "./components/OddajRzeczy";
+import {
+    HashRouter,
+    Switch,
+    Route,
+    Link,
+    NavLink
+} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      Hi
+      <HashRouter>
+          <div>
+              <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='/oddaj-rzeczy'>oddaj rzeczy</Link></li>
+                  {/*<li><Link to='/logowanie'>logowanie</Link></li>*/}
+                  {/*<li><Link to='/rejestracja'>rejestracja</Link></li>*/}
+                  {/*<li><Link to='/wylogowano'>wylogowano</Link></li>*/}
+              </ul>
+          </div>
+
+          <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/oddaj-rzeczy' component={OddajRzeczy}/>
+              {/*<Route exact path='/logowanie' component={Logowanie}/>*/}
+              {/*<Route exact path='/rejestracja' component={Rejestracja}/>*/}
+              {/*<Route exact path='/wylogowano' component={Wylogowano}/>*/}
+          </Switch>
+
+      </HashRouter>
     </div>
   );
 }
---
+++ src/components/OddajRzeczy.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function OddajRzeczy() {
+    return <h1>Komponent OddajRzeczy</h1>;
+}
