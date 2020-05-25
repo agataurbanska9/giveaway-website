3b0cc50efd02fb847fd8a0615f8043e98ebad1fd
Agata Urbańska <agata.urbanska9@gmail.com>
2020-05-25 11:32:07 +0200

    three columns section styling

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
 export function HomeThreeColumns() {
-    return <h1>HomeThreeColumns</h1>;
+    return (
+        <section className='home home--columns container'>
+            <div className='columns--content'>
+                <div className='column'>
+                    <h3>10</h3>
+                    <p>Oddanych worków</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci at atque autem consequatur dolores doloribus.</p>
+                </div>
+                <div className='column'>
+                    <h3>5</h3>
+                    <p>Wspartych organizacji</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci at atque autem consequatur dolores doloribus.</p>
+                </div>
+                <div className='column'>
+                    <h3>7</h3>
+                    <p>Zorganizowanych zbiórek</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci at atque autem consequatur dolores doloribus.</p>
+                </div>
+            </div>
+        </section>
+    )
 }
