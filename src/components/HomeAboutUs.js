dd794c10625927ba169b381c16d2f9b9cf694361
Agata Urbańska <agata.urbanska9@gmail.com>
2020-05-25 13:11:01 +0200

    about us styling

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import decoration from "../assets/Decoration.svg";
+import signature from  '../assets/Signature.svg';
+import people from '../assets/People.jpg';
 
 export function HomeAboutUs() {
     return (
-            <h1>HomeAboutUs</h1>
+
+        <section className='home home--aboutus container'>
+            <div className='aboutus--text'>
+                <p>O nas</p>
+                <img src={decoration} alt='Decoration'/>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img src={signature} alt='Signature'/>
+            </div>
+            <div className='aboutus--photo'>
+                <img src={people} alt='Group of people smiling' className='aboutus--img'/>
+            </div>
+        </section>
     );
 }
