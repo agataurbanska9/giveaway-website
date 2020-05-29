354851da29872dfd20c05a60d3ce12a6dc27ac19
Agata Urbańska <agata.urbanska9@gmail.com>
2020-05-29 15:51:02 +0200

    login elem styling

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import decoration from "../assets/Decoration.svg";
 
 export function LogIn() {
-    return <h1>LogIn</h1>;
+    return (
+        <section className='container login'>
+            <div className='login--form'>
+                <p>Zaloguj się</p>
+                <img src={decoration} alt='Decoration'/>
+                <form>
+                    <div className='labels'>
+                        <label>
+                            Email <br/>
+                            <input type='email' name='email'/>
+                        </label>
+                        <br/>
+                        <label>
+                            Hasło <br/>
+                            <input type='password' name='password'/>
+                        </label>
+
+                    </div>
+
+
+                    <input type="submit" value="Zaloguj się" className='btn btn--login-send'/>
+                    <button className='btn btn--registry'>Załóż konto</button>
+                </form>
+
+
+            </div>
+
+        </section>
+
+
+
+    );
 }
